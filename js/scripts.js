441b8ad61ba4739933404c6d77c8042fee5a3c20
rrigs <71404266+rrigs@users.noreply.github.com>
2020-10-14 17:26:55 -0400

    added objects to pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-alert('Hello world');
-
-let favoriteFood = 'Ice Cream';
-document.write(favoriteFood);
+let pokemonList = [
+    {
+        name: 'Bulbasaur',
+        height: 0.7,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Ivysaur',
+        height: 1,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Venusaur',
+        height: 2,
+        types: ['grass', 'poison']
+    },
+    {
+        name: 'Charmander',
+        height: 0.6,
+        types: 'fire'
+    },
+    {
+        name: 'Charmeleon',
+        height: 1.1,
+        types: 'fire'
+    },
+    {
+        name: 'Charizard',
+        height: 1.7,
+        types: ['fire', 'flying']
+    }
+]
