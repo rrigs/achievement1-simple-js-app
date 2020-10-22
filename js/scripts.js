66e08e2e597abb5890477d9d4e2a6defc69e93fc
rrigs <71404266+rrigs@users.noreply.github.com>
2020-10-22 17:19:21 -0400

    changed for loop to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@ let pokemonList = [
   {
     name: "Bulbasaur",
     height: 0.7,
-    types: ["grass", "speed"],
+    types: ["grass", "poison"],
   },
   {
     name: "Ivysaur",
     height: 1,
-    types: ["water", "speed"],
+    types: ["grass", "poison"],
   },
   {
     name: "Venusaur",
-    height: 2.2,
-    types: ["poison", "scary"],
+    height: 2,
+    types: ["grass", "poison"],
   },
   {
     name: "Charmander",
@@ -22,64 +22,21 @@ let pokemonList = [
   {
     name: "Charmeleon",
     height: 1.1,
-    types: ["flying"],
+    types: ["fire"],
   },
   {
     name: "Charizard",
     height: 1.7,
-    types: ["electric", "speed"],
+    types: ["fire", "flying"],
   },
 ];
 
-for (let i = 0; i < pokemonList.length; i++) {
-    // lists pokemon names and heights
-    document.write(pokemonList[i].name + " (height: " + pokemonList[i].height + ") ")
-    // highlights pokemon larger than 1.5
-    if (pokemonList[i].height > 1.5) {
-        document.write(" - Wow, that's big! ")
-    }
-    document.write("<br/>")
+function myLoopFunction(pokemon) {
+/*   let size =" ";
+  if (pokemonList.height > 1.5) {
+    size = "Wow, that's big!";
+  } */
+  document.write(pokemon.name + ' (height:' + pokemon.height + ') ' + pokemon.types + "</br>")
 }
 
-//console.log(pokemonList.length)
-
-/* for (let i = 0; i < pokemonList.length; i++) {
-  let size;
-  if (pokemonList[i].height > 2) {
-    size = "Wow, that's big!";
-  } else if (pokemonList[i].height < 2 && pokemonList[i].height > 1) {
-    size = "This is an average Pokemon.";
-  } else {
-    size = "This is a small Pokemon.";
-  }
-
-  let color;
-  for (let k = 0; k < pokemonList[i].types.length; k++) {
-    if (pokemonList[i].types[k] == "grass") {
-      color = '<span style="color: green;">';
-    } else if (pokemonList[i].types[k] == "fire") {
-      color = '<span style="color: red;">';
-    } else if (pokemonList[i].types[k] == "poison") {
-      color = '<span style="color: purple;">';
-    } else if (pokemonList[i].types[k] == "electric") {
-      color = '<span style="color: yellow;">';
-    } else if (pokemonList[i].types[k] == "flying") {
-      color = '<span style="color: lightblue;">';
-    } else if (pokemonList[i].types[k] == "water") {
-      color = '<span style="color: blue;">';
-    }
-  }
-  document.write(
-    '<div class="box">' +
-      pokemonList[i].name +
-      " (height: " +
-      pokemonList[i].height +
-      ") " +
-      size +
-      color +
-      "</br>" +
-      pokemonList[i].types +
-      "<br/>" +
-      "</div>"
-  );
-} */
+pokemonList.forEach(myLoopFunction);
